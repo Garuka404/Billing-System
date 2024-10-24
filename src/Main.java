02cc610ef438d9f415a922ba214dd53ee05c01f6
Garuka404 <garukagimhana@gmail.com>
2024-10-24 22:54:33 +0530

    Added ASCII art for system header in the main manu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("=======================================================");
+
+        System.out.println("__          __  _   " + "" + "");
+        System.out.println("\\ \\        / / | |    " + "" + "");
+        System.out.println(" \\ \\  /\\  / /_ | | ___ ___  _ __ ___   ___      " + "" + "");
+        System.out.println("  \\ \\/  \\/ / _ \\ |/ __/ _ \\| \'_ ` _ \\ / _ \\         " + "" + "");
+        System.out.println("   \\  /\\  /  __/ | (_| (_)|| | | | | |  __/                  " + "" + "");
+        System.out.println("    \\/  \\/ \\___|_|\\___\\___/|_| |_| | |\\___|  " + "" + "");
+
+        System.out.println("\n=======================================================");
+
+
+
+    }}
